ed780672cd500c327075d50d59eb67637ee160e5
mm1ord <1090485174@qq.com>
2026-04-02 22:19:46 +0800

    update all

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,7 +27,6 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
@@ -35,7 +34,8 @@ extern "Rust" {
 }
 
 mod Foo {
-    // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
+    #[link_name = "my_demo_function_alias"]
     fn my_demo_function(a: u32) -> u32 {
         a
     }
